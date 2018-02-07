5e6f96471fe20c5e1c92a7a08b9456daa2f7255f
x <j4c0bs@users.noreply.github.com>
2018-02-07 17:24:31 -0500

    set explicit module imports and added docstring

--- choppy/__init__.py
+++ choppy/__init__.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+Choppy public functions
+-----------------------
+chop, chop_encrypt, merge, decrypt_merge
+
+Choppy public module
+--------------------
+crypto
+
+
+copyright: (c) Jeremy Jacobs 2017-2018
+license: BSD-2-Clause see LICENSE.txt for details
+"""
+
 from choppy.chop import chop, chop_encrypt
 from choppy.merge import merge, decrypt_merge
+from choppy import crypto
 from choppy.version import VERSION as __version__
+
+__all__ = ['__version__', 'chop', 'chop_encrypt', 'merge', 'decrypt_merge', 'crypto']
